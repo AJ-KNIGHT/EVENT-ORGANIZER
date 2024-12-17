2a958e93f981671084c38d6b626269df0bb24193
AJ-KNIGHT <amalaj5025@gmail.com>
2024-12-17 21:10:54 +0530

    files updated

--- learning/eventpr/eventapp/views.py
+++ learning/eventpr/eventapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Event
+from .forms import BookingForm
 
 # Create your views here.
 def index(request):
@@ -6,8 +8,15 @@ def index(request):
 def about(request):
     return render(request,'about.html')
 def events(request):
-    return render(request,'events.html')
+    dict_eve={
+        'eve':Event.objects.all
+    }
+    return render(request,'events.html',dict_eve)
 def booking(request):
+    form=BookingForm()
+    dict_form={
+        'form':form
+    }
     return render(request,'booking.html')
 def contact(request):
     return render(request,'contact.html')
